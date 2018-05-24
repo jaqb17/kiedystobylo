190a33ae2f2621f16e64847dec47e00738f024ab
Sebastian Magnuszewski <32669076+smagnuszewski@users.noreply.github.com>
2018-05-24 10:19:19 +0200

    changes

--- DevilSoup/DevilSoup/Fireplace.cs
+++ DevilSoup/DevilSoup/Fireplace.cs
@@ -14,8 +14,9 @@ namespace DevilSoup
         public Rectangle barRectangle;
         public Texture2D texture { get; set; }
         public Vector2 position { get; set; }
-        private List<WoodenLog> LogsUnderCauldron;
+        private WoodenLog[] LogsUnderCauldron;
         public double fuelValue;
+        private const int maxLogsInFire = 4;
 
         public Fireplace(Vector2 _position, string texturePath, ContentManager content)
         {
@@ -23,7 +24,9 @@ namespace DevilSoup
             texture = content.Load<Texture2D>(texturePath);
             barRectangle = new Rectangle((int)_position.X, (int)_position.Y, 400, 400);
             fuelValue = 3;
-            LogsUnderCauldron = null;
+            LogsUnderCauldron = new WoodenLog[4];
+            for (int i = 0; i < maxLogsInFire; i++)
+                LogsUnderCauldron[i] = null;
         }
         public void fuelValueChange (int changeValue)
         {
@@ -51,27 +54,28 @@ namespace DevilSoup
         {
             //LogsUnderCauldron.Add
         }
-        public float calculateFireValue()
-        {
-            return LogsUnderCauldron.Count;
-        }
-        public void updateLogDecayRatio()
-        {
-            foreach (var log in LogsUnderCauldron)
-            {
-                log.decayValue -= 0.01;
-                if (log.decayValue <= 0)
-                    LogsUnderCauldron.Remove(log);
-            }
-        }
+        //public float calculateFireValue()
+        //{
+        //    return LogsUnderCauldron.Count;
+        //}
+        //public void updateLogDecayRatio()
+        //{
+        //    foreach (var log in LogsUnderCauldron)
+        //    {
+        //        log.decayValue -= 0.01;
+        //        if (log.decayValue <= 0)
+        //            LogsUnderCauldron.Remove(log);
+        //    }
+        //}
 
-        /*public void drawWoodenLogs()
+        public void drawWoodenLogs(GameTime gameTime, Matrix view, Matrix projection)
         {
-            foreach (var log in LogsUnderCauldron)
+          for (int i=0;i<maxLogsInFire;i++)
             {
-                if
+                if (LogsUnderCauldron[i] != null)
+                    LogsUnderCauldron[i].drawWoodenLog(gameTime, view, projection);
             }
         }
-        */
+        
     }   
 }
