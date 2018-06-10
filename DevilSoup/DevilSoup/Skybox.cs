0e0b1f8b8d43ea5174797371983bb5d20a647e4c
Unknown <olickav@gmail.com>
2018-06-10 21:05:21 +0200

    gitflow-feature-stash: rak

---
+++ DevilSoup/DevilSoup/Skybox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace DevilSoup
+{
+    class Skybox
+    {
+        private Model skyBox;
+        /// <summary>
+        /// The actual skybox texture
+        /// </summary>
+        private TextureCube skyBoxTexture;
+
+        /// <summary>
+        /// The effect file that the skybox will use to render
+        /// </summary>
+        private Effect skyBoxEffect;
+
+        /// <summary>
+        /// The size of the cube, used so that we can resize the box
+        /// for different sized environments.
+        /// </summary>
+        private float size = 50f;
+        /// <summary>
+        /// Creates a new skybox
+        /// </summary>
+        /// <param name="skyboxTexture">the name of the skybox texture to use</param>
+        public Skybox(string skyboxTexture, ContentManager Content)
+        {
+            skyBox = Content.Load<Model>("Assets/Skybox/cube");
+            skyBoxTexture = Content.Load<TextureCube>(skyboxTexture);
+            skyBoxEffect = Content.Load<Effect>("Skyboxes/Skybox");
+        }
+    }
+}
