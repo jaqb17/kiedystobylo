6ccb3d28ebcc1127482b5370911b5a8abd04abc3
jaqb17 <jaqb17@gmail.com>
2018-06-25 17:12:48 +0200

    model czachy dziala w obj

--- DevilSoup/DevilSoup/SkullRing.cs
+++ DevilSoup/DevilSoup/SkullRing.cs
@@ -22,26 +22,35 @@ namespace DevilSoup
            
 
             transforms = new List<Matrix>();
-            skullAsset = new Asset(content, "Assets/Souls/TreadingWater",
+            skullAsset = new Asset(content, "Assets/Czacha/czachaobj",
                                             "Assets/Czacha/grzesznik2txtr_Albedo",
                                             "Assets/Czacha/grzesznik2txtr_Normal",
                                             "Assets /Czacha/grzesznik2txtr_Specular",
                                             camera);
+            skullAsset.setLightDirection(new Vector3(0, -1, -1));
+
+            //skullAsset = new Asset(content, "Assets/test/vs",
+            //                               "Assets/test/vsc",
+            //                               "Assets/test/vsn",
+            //                               "Assets/test/vss",
+            //                               camera);
+
+
 
             //na godzinie 12 jest transform 0 i dalej zgodnie z zegarem
 
-            transforms.Add(Matrix.CreateTranslation(0, 30, -40)); // 0
-            transforms.Add(Matrix.CreateTranslation(35, 30, -35)); // 1
+            transforms.Add(Matrix.CreateTranslation(0, 30, -35)); // 0
+            transforms.Add(Matrix.CreateTranslation(35, 30, -25)); // 1
             transforms.Add(Matrix.CreateTranslation(50, 30, 0)); // 2
             transforms.Add(Matrix.CreateTranslation(35, 30, 35)); // 3
-            transforms.Add(Matrix.CreateTranslation(0, 30, 50)); // 4
+            transforms.Add(Matrix.CreateTranslation(0, 30, 55)); // 4
             transforms.Add(Matrix.CreateTranslation(-35, 30, 35)); // 5
             transforms.Add(Matrix.CreateTranslation(-50, 30, 0)); // 6
-            transforms.Add(Matrix.CreateTranslation(-35, 30, -35)); // 7
+            transforms.Add(Matrix.CreateTranslation(-35, 30, -25)); // 7
 
 
             for (int i = 0; i < transforms.Count; i++)
-                transforms[i] = Matrix.CreateScale(.7f) * transforms[i];
+                transforms[i] = Matrix.CreateScale(1f) * transforms[i];
 
         }
 
@@ -49,10 +58,10 @@ namespace DevilSoup
         public void render(Camera camera)
         {
             int howManyToDraw = Convert.ToInt32(Math.Floor(transforms.Count * danceArea.combo.getComboStatus()));
-            Debug.WriteLine(danceArea.combo.getComboStatus());
+           // Debug.WriteLine(danceArea.combo.getComboStatus());
             for (int i = 0; i < howManyToDraw; i++)
             {
-                skullAsset.world = Matrix.Identity * Matrix.CreateRotationY(MathHelper.ToRadians(rotation));
+                skullAsset.world = Matrix.Identity * Matrix.CreateRotationY(MathHelper.ToRadians(rotation)) * Matrix.CreateRotationX(MathHelper.ToRadians(rotation))* Matrix.CreateRotationZ(MathHelper.ToRadians(rotation));
                 skullAsset.world = skullAsset.world*transforms[i];
                 skullAsset.SimpleDraw(camera.view, camera.projection);
             }
